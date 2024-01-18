f44232bdeb6d59c208100fae9bc12115360c0c00
Milind Renjit <milindr@ad.unc.edu>
2024-01-18 18:25:46 -0500

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,6 @@
+"""My First Program for COMP110."""
+
+_author_= "730531221"
+
+print("Hello, world.")
+print("Wake up and say, hello world!")
---
+++ lessons/input.py
@@ -0,0 +1,3 @@
+"""Practice with User Input"""
+
+input("What is your name?")
